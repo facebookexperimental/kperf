a6e6ea61d133fa64deba03871b09eee6f96178a8
Sheldon Wu <xzw2048@gmail.com>
2025-09-08 20:55:06 -0700

    devmem: switch to CUDA runtime cudaMalloc
    Summary:
    CUDA runtime API is easier to manage cuda context. and easier to do IPC memory share
    
    Test Plan:
    == Source
      Tx116.073 Gbps (145107189760 bytes in 10001102 usec)
      Rx  0.000 Gbps (0 bytes in 10001102 usec)
      TCP retrans reord rtt rttvar d_ce snd_wnd cwnd
                0     0 2874     21    0 52230144 10733
      net CPU 56: usr: 0.00% sys: 0.00% idle:54.12% iow: 0.00% irq: 4.32% sirq:41.54%
      app CPU 57: usr: 0.97% sys:52.76% idle:45.71% iow: 0.00% irq: 0.43% sirq: 0.10%
    == Target
      Tx  0.000 Gbps (0 bytes in 10003312 usec)
      Rx116.026 Gbps (145080635392 bytes in 10003312 usec)
      TCP retrans reord rtt rttvar d_ce snd_wnd cwnd
                0     0  55     28    0   63488   10
      net CPU  0: usr: 0.00% sys: 0.00% idle: 0.09% iow: 0.00% irq: 3.39% sirq:96.50%
      app CPU  1: usr: 1.80% sys:97.79% idle: 0.00% iow: 0.00% irq: 0.30% sirq: 0.10%
    
    Tags:
    Signed-off-by: Sheldon Wu <xzw2048@gmail.com>

--- devmem.c
+++ devmem.c
@@ -637,8 +637,6 @@ static struct memory_buffer *cuda_alloc(size_t size)
 {
 	struct memory_buffer *mem;
 	size_t page_size;
-	CUdevice dev;
-	int devnum;
 	int ret;
 
 	page_size = sysconf(_SC_PAGESIZE);
@@ -650,24 +648,12 @@ static struct memory_buffer *cuda_alloc(size_t size)
 	mem = calloc(1, sizeof(*mem));
 	if (!mem)
 		return NULL;
-
-	ret = cudaGetDevice(&devnum);
-	if (ret != cudaSuccess)
-		goto free_mem;
-
-	ret = cuDeviceGet(&dev, devnum);
-	if (ret != CUDA_SUCCESS)
-		goto free_mem;
-
-	ret = cuCtxCreate(&mem->cuda.ctx, 0, dev);
-	if (ret != CUDA_SUCCESS)
-		goto free_mem;
-
+	memset(mem, 0, sizeof(*mem));
 	mem->size = size;
 
-	ret = cuMemAlloc((CUdeviceptr *)&mem->buf_mem, size);
+	ret = cudaMalloc((void *)&mem->buf_mem, size);
 	if (ret != CUDA_SUCCESS)
-		goto destroy_ctx;
+		goto free_mem;
 
 	ret = cuMemGetHandleForAddressRange((void *)&mem->fd, ((CUdeviceptr)mem->buf_mem),
 					    size, CU_MEM_RANGE_HANDLE_TYPE_DMA_BUF_FD,
@@ -678,11 +664,8 @@ static struct memory_buffer *cuda_alloc(size_t size)
 	return mem;
 
 free_cuda:
-	if (cuMemFree((CUdeviceptr)mem->buf_mem) != CUDA_SUCCESS)
-		warnx("cuMemFree() failed");
-destroy_ctx:
-	if (cuCtxDestroy(mem->cuda.ctx) != CUDA_SUCCESS)
-		warnx("cuCtxDestroy() failed");
+	if (cudaFree(mem->buf_mem) != cudaSuccess)
+		warnx("cudaFree() failed");
 free_mem:
 	free(mem);
 
@@ -693,12 +676,9 @@ static void cuda_free(struct memory_buffer *mem)
 {
 	if (mem->fd)
 		close(mem->fd);
-	if (mem->buf_mem) {
-		munmap(mem->buf_mem, mem->size);
-		cuMemFree((CUdeviceptr)mem->buf_mem);
-	}
+	if (mem->buf_mem)
+		cudaFree(mem->buf_mem);
 
-	cuCtxDestroy(mem->cuda.ctx);
 	free(mem);
 }
 
@@ -707,9 +687,9 @@ void cuda_memcpy_to_device(struct memory_buffer *dst, size_t off,
 {
 	int ret;
 
-	ret = cuMemcpyHtoD((CUdeviceptr)(dst->buf_mem + off), src, n);
+	ret = cudaMemcpy((void *)(dst->buf_mem + off), src, n, cudaMemcpyHostToDevice);
 	if (ret != CUDA_SUCCESS)
-		warnx("cuMemcpyHtoD() failed");
+		warnx("cudaMemcpy() failed");
 }
 
 static struct memory_provider cuda_memory_provider = {
