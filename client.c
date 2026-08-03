facebookexperimental/kperf

commit 7d5690b692997c5a4f7885db99ab769eb58cc145

devmem: add support for TX with udmabuf
To test devmem TX with receive validation (normal sockets), this was
tested using:

            ./client \
                    --devmem-tx \
                    --dmabuf-tx-size-mb 2048 \
                    --src $SRC_IP \
                    --dst $DST_IP \
                    --validate yes \
                    --time 10 \
                    --pin-off 4 \
                    --read-size 4194304 \
                    --write-size 4194304

Output:

I   client.c: Connected 5:cpu 56 | 5:cpu 1
I   client.c: Connected 3:cpu 57 | 3:cpu 2
I   client.c: Connected 1:cpu 58 | 1:cpu 0
I   client.c: Connected 9:cpu 59 | 9:cpu 3
client: == Source
client:   Tx 39.724 Gbps (49660559360 bytes in 10001037 usec)
client:   Tx 40.932 Gbps (51170508800 bytes in 10001037 usec)
client:   Tx 39.691 Gbps (49618616320 bytes in 10001037 usec)
client:   Tx 39.993 Gbps (49996103680 bytes in 10001037 usec)

[... snip ...]

client: == Target
client:   Tx  0.000 Gbps (0 bytes in 12369345 usec)
client:   Tx  0.000 Gbps (0 bytes in 12369345 usec)
client:   Tx  0.000 Gbps (0 bytes in 12369345 usec)
client:   Tx  0.000 Gbps (0 bytes in 12369345 usec)
client:   Rx 32.118 Gbps (49660559360 bytes in 12369345 usec)
client:   Rx 33.095 Gbps (51170508800 bytes in 12369345 usec)
client:   Rx 32.091 Gbps (49618616320 bytes in 12369345 usec)
client:   Rx 32.335 Gbps (49996103680 bytes in 12369345 usec)
client:   TCP retrans reord rtt rttvar d_ce snd_wnd cwnd
client:             0     0  41     22    0   63488   10
client:             0     0  51     28    0   63488   10
client:             0     0  41     21    0   63488   10
client:             0     0  20      7    0   63488   10
client:   net CPU  1: usr: 0.00% sys: 0.08% idle:74.65% iow: 0.00% irq: 0.89% sirq:24.36%
client:   app CPU  5: usr:20.77% sys:59.66% idle:18.99% iow: 0.00% irq: 0.48% sirq: 0.08%
client:   net CPU  2: usr: 0.08% sys: 0.08% idle:75.51% iow: 0.00% irq: 0.90% sirq:23.42%

client.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
@@ -27,6 +28,7 @@ static struct {
 	bool devmem_rx;
 	enum memory_provider_type devmem_rx_memory;
 	struct pci_dev devmem_dst_dev;
+	bool devmem_tx;
 	bool msg_zerocopy;
 	bool tls;
 	bool tls_rx;
@@ -56,6 +58,7 @@ static struct {
 	unsigned int max_pace;
 	char *tcp_cong_ctrl;
 	unsigned int dmabuf_rx_size_mb;
+	unsigned int dmabuf_tx_size_mb;
 	unsigned int num_rx_queues;
 	bool validate;
 } opt = {
@@ -75,6 +78,7 @@ static struct {
 	.n_conns = 1,
 	/* 128M is enough to drive one queue at 200G */
 	.dmabuf_rx_size_mb = 128,
+	.dmabuf_tx_size_mb = 128,
 	.num_rx_queues = 1,
 	.devmem_rx_memory = MEMORY_PROVIDER_HOST,
 	.devmem_dst_dev = {
@@ -243,6 +247,9 @@ static const struct opt_table opts[] = {
 		     "Select the memory provider for TCP Devmem RX"),
 	OPT_WITH_ARG("--dmabuf-rx-size-mb <arg>", opt_set_uintval, opt_show_uintval,
 		     &opt.dmabuf_rx_size_mb, "Size of RX dmabuf for TCP Devmem mode"),
+	OPT_WITH_ARG("--dmabuf-tx-size-mb <arg>", opt_set_uintval, opt_show_uintval,
+		     &opt.dmabuf_tx_size_mb, "Size of TX dmabuf for TCP Devmem mode"),
+	OPT_WITHOUT_ARG("--devmem-tx", opt_set_bool, &opt.devmem_tx, "Use TCP Devmem on transmit"),
 	OPT_WITH_ARG("--num-rx-queues <arg>", opt_set_uintval, opt_show_uintval,
 		     &opt.num_rx_queues, "Number of RX queues for TCP Devmem mode"),
 	OPT_WITH_ARG("--validate <yes|no>", opt_set_bool_arg, NULL, &opt.validate,
@@ -640,6 +647,38 @@ dump_result_machine(struct kpm_test_results *result, const char *dir,
 	printf(local ? "," : "\n");
 }
 
+/* copied from devmem.c */
+static void inet_to_inet6(struct sockaddr *addr, struct sockaddr_in6 *out)
+{
+	out->sin6_addr.s6_addr32[3] = ((struct sockaddr_in6 *)addr)->sin6_addr.s6_addr32[0];
+	out->sin6_addr.s6_addr32[0] = 0;
+	out->sin6_addr.s6_addr32[1] = 0;
+	out->sin6_addr.s6_addr16[4] = 0;
+	out->sin6_addr.s6_addr16[5] = 0xffff;
+	out->sin6_family = AF_INET6;
+}
+
+int inet_sockaddr(const char *str, struct sockaddr_in6 *out)
+{
+	struct sockaddr_in *sa4;
+	struct sockaddr_in6 tmp;
+
+	out->sin6_family = AF_INET6;
+	if (inet_pton(AF_INET6, str, &(out->sin6_addr)) == 1) {
+		out->sin6_family = AF_INET6;
+		return 0;
+	}
+
+	sa4 = (struct sockaddr_in *)&tmp;
+	if (inet_pton(AF_INET, str, &(sa4->sin_addr)) == 1) {
+		sa4->sin_family = AF_INET;
+		inet_to_inet6((void *)sa4, out);
+		return 0;
+	}
+
+	return -1;
+}
+
 int main(int argc, char *argv[])
 {
 	enum kpm_rx_mode rx_mode = KPM_RX_MODE_SOCKET;
@@ -652,6 +691,7 @@ int main(int argc, char *argv[])
 	__u32 *src_wrk_id, *dst_wrk_id;
 	struct sockaddr_in6 conn_addr;
 	__u32 src_tst_id, dst_tst_id;
+	struct sockaddr_in6 src_addr;
 	struct addrinfo *addr;
 	struct kpm_test *test;
 	unsigned int i;
@@ -687,6 +727,9 @@ int main(int argc, char *argv[])
 			errx(1, "Cross-pin only works with 2 connections");
 	}
 
+	if (inet_sockaddr(opt.src, &src_addr) < 0)
+		errx(1, "failed to get sockaddr from %s\n", opt.src);
+
 	if (opt.msg_trunc && opt.validate)
 		errx(1, "--msg-trunc and --validate yes are mutually exclusive");
 
@@ -702,8 +745,13 @@ int main(int argc, char *argv[])
 	if (opt.validate && opt.devmem_rx_memory == MEMORY_PROVIDER_CUDA)
 		errx(1, "--devmem-rx-memory cuda does not support --validate yes");
 
+	if (opt.msg_zerocopy && opt.devmem_tx)
+		errx(1, "--msg-zerocopy and --devmem-tx are mutually exclusive");
+
 	if (opt.msg_zerocopy)
 		tx_mode = KPM_TX_MODE_SOCKET_ZEROCOPY;
+	else if (opt.devmem_tx)
+		tx_mode = KPM_TX_MODE_DEVMEM;
 
 	src_wrk_id = calloc(opt.n_conns, sizeof(*src_wrk_id));
 	dst_wrk_id = calloc(opt.n_conns, sizeof(*dst_wrk_id));
@@ -744,15 +792,15 @@ int main(int argc, char *argv[])
 	}
 
 	if (kpm_req_mode(dst, rx_mode, tx_mode, opt.dmabuf_rx_size_mb,
-			 opt.num_rx_queues, opt.validate,
-			 opt.devmem_rx_memory, &opt.devmem_dst_dev) < 0) {
+			 opt.dmabuf_tx_size_mb, opt.num_rx_queues, opt.validate,
+			 opt.devmem_rx_memory, &opt.devmem_dst_dev, NULL) < 0) {
 		warnx("Failed setup destination mode");
 		goto out;
 	}
 
 	if (kpm_req_mode(src, rx_mode, tx_mode, opt.dmabuf_rx_size_mb,
-			 opt.num_rx_queues, opt.validate,
-			 opt.devmem_rx_memory, NULL) < 0) {
+			 opt.dmabuf_tx_size_mb, opt.num_rx_queues, opt.validate,
+			 opt.devmem_rx_memory, NULL, &src_addr) < 0) {
 		warnx("Failed setup source mode");
 		goto out;
 	}

devmem.c
@@ -304,6 +304,47 @@ out:
 	return ret;
 }
 
+static int bind_tx_queue(unsigned int ifindex, unsigned int dmabuf_fd,
+			 struct ynl_sock *ys)
+{
+	struct netdev_bind_tx_req *req = NULL;
+	struct netdev_bind_tx_rsp *rsp = NULL;
+	int ret;
+
+	req = netdev_bind_tx_req_alloc();
+	if (!req) {
+		warnx("netdev_bind_tx_req_alloc() failed");
+		return -1;
+	}
+	netdev_bind_tx_req_set_ifindex(req, ifindex);
+	netdev_bind_tx_req_set_fd(req, dmabuf_fd);
+
+	rsp = netdev_bind_tx(ys, req);
+	if (!rsp) {
+		warnx("netdev_bind_tx");
+		ret = -1;
+		goto free_req;
+	}
+
+	if (!rsp->_present.id) {
+		warnx("id not present");
+		ret = -1;
+		goto free_rsp;
+	}
+
+	ret = rsp->id;
+	netdev_bind_tx_req_free(req);
+	netdev_bind_tx_rsp_free(rsp);
+
+	return ret;
+
+free_rsp:
+	netdev_bind_tx_rsp_free(rsp);
+free_req:
+	netdev_bind_tx_req_free(req);
+	return ret;
+}
+
 #define UDMABUF_LIMIT_PATH "/sys/module/udmabuf/parameters/size_limit_mb"
 
 static int udmabuf_check_size(size_t size_mb)
@@ -474,6 +515,7 @@ static struct memory_provider udmabuf_memory_provider = {
 };
 
 static struct memory_provider *rxmp;
+static struct memory_provider *txmp;
 
 #ifdef USE_CUDA
 
@@ -928,4 +970,95 @@ ssize_t devmem_recv(int fd, struct connection_devmem *conn,
 	}
 
 	return n;
+}
+
+int devmem_sendmsg(int fd, int dmabuf_id, size_t off, size_t n)
+{
+	char ctrl_data[CMSG_SPACE(sizeof(int))];
+	struct msghdr msg = { 0 };
+	struct cmsghdr *cmsg;
+	struct iovec iov;
+
+	iov.iov_base = (void *)off;
+	iov.iov_len = n;
+
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	msg.msg_control = ctrl_data;
+	msg.msg_controllen = sizeof(ctrl_data);
+
+	cmsg = CMSG_FIRSTHDR(&msg);
+	cmsg->cmsg_level = SOL_SOCKET;
+	cmsg->cmsg_type = SCM_DEVMEM_DMABUF;
+	cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+	*((int *)CMSG_DATA(cmsg)) = dmabuf_id;
+
+	return sendmsg(fd, &msg, MSG_ZEROCOPY);
+}
+
+int devmem_setup_tx(struct session_state_devmem *devmem, int fd)
+{
+	char ifname[IFNAMSIZ] = {};
+	struct ynl_error yerr;
+	int ifindex;
+	int ret;
+
+	txmp = get_memory_provider(devmem->tx_provider);
+	if (!txmp)
+		return -1;
+
+	if (txmp->dev_init && txmp->dev_init(&devmem->tx_dev) < 0)
+		return -1;
+
+	devmem->tx_mem = txmp->alloc(devmem->dmabuf_tx_size_mb * 1024 * 1024);
+	if (!devmem->tx_mem) {
+		warnx("Failed to allocate devmem tx buffer");
+		return -1;
+	}
+
+	txmp->memcpy_to_device(devmem->tx_mem, 0, patbuf, sizeof(patbuf));
+
+	ifindex = find_iface(&devmem->addr, ifname);
+	if (ifindex < 0) {
+		warnx("Failed to resolve ifindex: %s", strerror(-ifindex));
+		return -1;
+	}
+
+	devmem->ys = ynl_sock_create(&ynl_netdev_family, &yerr);
+	if (!devmem->ys) {
+		warnx("Failed to setup YNL socket: %s", yerr.msg);
+		return -1;
+	}
+
+	devmem->tx_mem->dmabuf_id = bind_tx_queue(ifindex, devmem->tx_mem->fd, devmem->ys);
+	if (devmem->tx_mem->dmabuf_id < 0) {
+		warnx("Failed to bind TX queue dmabuf: %d\n", devmem->tx_mem->dmabuf_id);
+		ret = -1;
+		goto sock_destroy;
+	}
+
+	if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, ifname, IFNAMSIZ)) {
+		warn("failed to bind device to socket");
+		ret = -1;
+		goto sock_destroy;
+	}
+
+	return 0;
+
+sock_destroy:
+	ynl_sock_destroy(devmem->ys);
+	devmem->ys = NULL;
+	return ret;
+}
+
+void devmem_teardown_tx(struct session_state_devmem *devmem)
+{
+	if (txmp)
+		txmp->free(devmem->tx_mem);
+
+	if (devmem->ys) {
+		ynl_sock_destroy(devmem->ys);
+		devmem->ys = NULL;
+	}
 }

proto.c
@@ -181,21 +181,26 @@ int kpm_send_tcp_cc(int fd, __u32 id, char *cc_name)
 }
 
 int kpm_send_mode(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
-		  __u32 dmabuf_rx_size_mb, __u32 num_rx_queues, __u8 validate,
+		  __u32 dmabuf_rx_size_mb, __u32 dmabuf_tx_size_mb,
+		  __u32 num_rx_queues, __u8 validate,
 		  enum memory_provider_type rx_provider,
-		  struct pci_dev *dev)
+		  struct pci_dev *dev, struct sockaddr_in6 *addr)
 {
 	struct kpm_mode msg = {};
 
 	msg.rx_mode = rx_mode;
 	msg.tx_mode = tx_mode;
 	msg.dmabuf_rx_size_mb = dmabuf_rx_size_mb;
+	msg.dmabuf_tx_size_mb = dmabuf_tx_size_mb;
 	msg.num_rx_queues = num_rx_queues;
 	msg.validate = validate;
 	msg.rx_provider = rx_provider;
 
 	if (dev)
 		memcpy(&msg.dev, dev, sizeof(msg.dev));
+
+	if (addr)
+		memcpy(&msg.addr, addr, sizeof(msg.addr));
 
 	return kpm_send(fd, &msg.hdr, sizeof(msg), KPM_MSG_TYPE_MODE);
 }
@@ -465,15 +470,17 @@ kpm_req_tcp_cc(int fd, __u32 conn_id, char *cc_name)
 
 int
 kpm_req_mode(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
-	     __u32 dmabuf_rx_size_mb, __u32 num_rx_queues, __u8 validate,
+	     __u32 dmabuf_rx_size_mb, __u32 dmabuf_tx_size_mb,
+	     __u32 num_rx_queues, __u8 validate,
 	     enum memory_provider_type rx_provider,
-	     struct pci_dev *dev)
+	     struct pci_dev *dev, struct sockaddr_in6 *addr)
 {
 	struct kpm_empty *repl;
 	int id;
 
-	id = kpm_send_mode(fd, rx_mode, tx_mode, dmabuf_rx_size_mb, num_rx_queues, validate,
-			   rx_provider, dev);
+	id = kpm_send_mode(fd, rx_mode, tx_mode, dmabuf_rx_size_mb, dmabuf_tx_size_mb,
+			   num_rx_queues, validate, rx_provider, dev,
+			   addr);
 	if (id < 0) {
 		warnx("Failed to request mode");
 		return id;

proto.h
@@ -131,6 +131,7 @@ enum kpm_rx_mode {
 enum kpm_tx_mode {
 	KPM_TX_MODE_SOCKET,
 	KPM_TX_MODE_SOCKET_ZEROCOPY,
+	KPM_TX_MODE_DEVMEM,
 };
 
 enum memory_provider_type {
@@ -157,7 +158,9 @@ struct kpm_mode {
 	enum memory_provider_type rx_provider;
 	struct pci_dev dev;
 	__u32 dmabuf_rx_size_mb;
+	__u32 dmabuf_tx_size_mb;
 	__u32 num_rx_queues;
+	struct sockaddr_in6 addr;
 
 	__u8 validate;
 };
@@ -294,9 +297,10 @@ int kpm_send_tls(int fd, __u32 conn_id, __u32 dir_mask,
 int kpm_send_max_pacing(int fd, __u32 id, __u32 max_pace);
 int kpm_send_tcp_cc(int fd, __u32 id, char *cc_name);
 int kpm_send_mode(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
-		  __u32 dmabuf_rx_size_mb, __u32 num_rx_queues, __u8 validate,
+		  __u32 dmabuf_rx_size_mb, __u32 dmabuf_tx_size_mb,
+		  __u32 num_rx_queues, __u8 validate,
 		  enum memory_provider_type rx_provider,
-		  struct pci_dev *dev);
+		  struct pci_dev *dev, struct sockaddr_in6 *addr);
 int kpm_send_pin_worker(int fd, __u32 id, __u32 cpu);
 
 void kpm_reply_error(int fd, struct kpm_header *hdr, __u16 error);
@@ -320,9 +324,10 @@ int kpm_req_tls(int fd, __u32 conn_id, __u32 dir_mask,
 int kpm_req_pacing(int fd, __u32 conn_id, __u32 max_pace);
 int kpm_req_tcp_cc(int fd, __u32 conn_id, char *cc_name);
 int kpm_req_mode(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
-		 __u32 dmabuf_rx_size_mb, __u32 num_rx_queues, __u8 validate,
+		 __u32 dmabuf_rx_size_mb, __u32 dmabuf_tx_size_mb,
+		 __u32 num_rx_queues, __u8 validate,
 		 enum memory_provider_type rx_provider,
-		 struct pci_dev *dev);
+		 struct pci_dev *dev, struct sockaddr_in6 *addr);
 int kpm_req_disconnect(int fd, __u32 connection_id);
 
 #endif /* PROTO_H */

server.h
@@ -86,27 +86,43 @@ struct connection_devmem {
 struct session_state_devmem {
 	struct ynl_sock *ys;
 	char ifname[IFNAMSIZ];
+
+	/* RX */
 	struct memory_buffer *mem;
 	int rss_context;
+
+	/* TX */
+	struct memory_buffer *tx_mem;
+	struct pci_dev tx_dev;
+	__u32 dmabuf_tx_size_mb;
+	enum memory_provider_type tx_provider;
+	struct sockaddr_in6 addr;
 };
 
 struct worker_state_devmem {
 	struct memory_buffer *mem;
+	int dmabuf_id;
 };
 
 struct server_session *
 server_session_spawn(int fd, struct sockaddr_in6 *addr, socklen_t *addrlen);
 
 void NORETURN pworker_main(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
-                           struct memory_buffer *devmem, bool validate);
+			   struct memory_buffer *devmem, bool validate,
+			   int dmabuf_id);
 
 int devmem_setup(struct session_state_devmem *devmem, int fd,
 		 size_t dmabuf_size, int num_queues,
 		 enum memory_provider_type provider, struct pci_dev *dev);
 int devmem_teardown(struct session_state_devmem *devmem);
+void devmem_teardown_tx(struct session_state_devmem *devmem);
 int devmem_release_tokens(int fd, struct connection_devmem *conn);
 ssize_t devmem_recv(int fd, struct connection_devmem *conn,
 		    unsigned char *rxbuf, size_t chunk, struct memory_buffer *mem,
 		    int rep, __u64 tot_recv, bool validate);
+int devmem_sendmsg(int fd, int dmabuf_id, size_t off, size_t n);
+void devmem_teardown_conn(struct connection_devmem *devmem);
+int devmem_prepare_connect(int fd, struct sockaddr_in6 *src, struct session_state_devmem *devmem);
 
+int devmem_setup_tx(struct session_state_devmem *devmem, int fd);
 #endif /* SERVER_H */

server_session.c
@@ -246,6 +246,14 @@ server_msg_connect(struct session_state *self, struct kpm_header *hdr)
 		goto err_close;
 	}
 
+	if (self->tx_mode == KPM_TX_MODE_DEVMEM) {
+		if (devmem_setup_tx(&self->devmem, cfd) < 0) {
+			warnx("Failed to setup devmem TX");
+			goto err_close;
+		}
+
+	}
+
 	ret = connect(cfd, (void *)&req->addr, req->len);
 	if (ret < 0) {
 		warn("Failed to connect");
@@ -528,6 +536,13 @@ server_msg_mode(struct session_state *self, struct kpm_header *hdr)
 	self->tx_mode = req->tx_mode;
 	self->validate = req->validate;
 
+	if (!self->tcp_sock && (req->tx_mode == KPM_TX_MODE_DEVMEM)) {
+		self->devmem.tx_provider = MEMORY_PROVIDER_HOST;
+		self->devmem.dmabuf_tx_size_mb = req->dmabuf_tx_size_mb;
+		memcpy(&self->devmem.tx_dev, &req->dev, sizeof(self->devmem.tx_dev));
+		memcpy(&self->devmem.addr, &req->addr, sizeof(self->devmem.addr));
+	}
+
 	if (kpm_reply_empty(self->main_sock, hdr) < 1) {
 		warnx("Reply failed");
 		goto err_quit;
@@ -562,9 +577,11 @@ server_msg_spawn_pworker(struct session_state *self, struct kpm_header *hdr)
 		goto err_free;
 	}
 	if (!pwrk->pid) {
+		int dmabuf_id = self->devmem.tx_mem ? self->devmem.tx_mem->dmabuf_id : -1;
+
 		close(p[0]);
 		pworker_main(p[1], self->rx_mode, self->tx_mode, self->devmem.mem,
-			     self->validate);
+			     self->validate, dmabuf_id);
 		exit(1);
 	}
 
@@ -1034,6 +1051,8 @@ static void server_session_loop(int fd)
 	}
 	if (self.tcp_sock && self.rx_mode == KPM_RX_MODE_DEVMEM)
 		devmem_teardown(&self.devmem);
+	if (!self.tcp_sock && self.tx_mode == KPM_TX_MODE_DEVMEM)
+		devmem_teardown_tx(&self.devmem);
 }
 
 static NORETURN void server_session(int fd)

worker.c
@@ -324,7 +324,7 @@ worker_msg_test(struct worker_state *self, struct kpm_header *hdr)
 		else
 			conn->to_recv = len;
 
-		zc = self->tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY;
+		zc = self->tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY || self->tx_mode == KPM_TX_MODE_DEVMEM;
 		if (setsockopt(conn->fd, SOL_SOCKET, SO_ZEROCOPY, &zc, sizeof(zc))) {
 			warnx("Failed to set SO_ZEROCOPY");
 			self->quit = 1;
@@ -575,16 +575,23 @@ worker_handle_send(struct worker_state *self, struct connection *conn,
 		   unsigned int events)
 {
 	unsigned int rep = max_t(int, 10, conn->to_send / conn->write_size + 1);
-	bool msg_zerocopy = self->tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY;
+	bool msg_zerocopy = self->tx_mode == KPM_TX_MODE_SOCKET_ZEROCOPY || self->tx_mode == KPM_TX_MODE_DEVMEM;
 	int flags = msg_zerocopy ? MSG_ZEROCOPY : 0;
 
 	while (rep--) {
-		void *src = &patbuf[conn->tot_sent % PATTERN_PERIOD];
 		size_t chunk;
+		void *src;
 		ssize_t n;
 
 		chunk = min_t(size_t, conn->write_size, conn->to_send);
-		n = send(conn->fd, src, chunk, MSG_DONTWAIT | flags);
+
+		if (self->tx_mode == KPM_TX_MODE_DEVMEM) {
+			n = devmem_sendmsg(conn->fd, self->devmem.dmabuf_id,
+					   conn->tot_sent % PATTERN_PERIOD, chunk);
+		} else {
+			src = &patbuf[conn->tot_sent % PATTERN_PERIOD];
+			n = send(conn->fd, src, chunk, MSG_DONTWAIT | flags);
+		}
 		if (n == 0) {
 			warnx("zero send chunk:%zd to_send:%lld to_recv:%lld",
 			      chunk, conn->to_send, conn->to_recv);
@@ -723,14 +730,14 @@ worker_handle_conn(struct worker_state *self, int fd, unsigned int events)
 /* == Main loop == */
 
 void NORETURN pworker_main(int fd, enum kpm_rx_mode rx_mode, enum kpm_tx_mode tx_mode,
-			   struct memory_buffer *devmem, bool validate)
+			   struct memory_buffer *devmem, bool validate, int dmabuf_id)
 {
 	struct worker_state self = {
 		.main_sock = fd,
 		.rx_mode = rx_mode,
 		.tx_mode = tx_mode,
 		.validate = validate,
-		.devmem = { .mem = devmem },
+		.devmem = { .mem = devmem, .dmabuf_id = dmabuf_id },
 	};
 	struct epoll_event ev, events[32];
 	int i, nfds;

commit 8e8550937d78a668fa99f8144274b2f4e9e07849

client: fix resolving DNS host names for --src
inet_sockaddr() only accepted numeric addresses, but it is called
unconditionally on opt.src. Passing a host name to --src aborts at
startup. Prior to the breakage, host names were handled by
net_client_lookup().

Resolve with net_client_lookup(), like we already do for the control
connections, which accepts both numeric addresses and host names.

Fixes: 7d5690b69299 ("devmem: add support for TX with udmabuf")
Signed-off-by: Bobby Eshleman <bobbyeshleman@meta.com>

client.c
@@ -701,23 +701,35 @@ static void inet_to_inet6(struct sockaddr *addr, struct sockaddr_in6 *out)
 
 int inet_sockaddr(const char *str, struct sockaddr_in6 *out)
 {
-	struct sockaddr_in *sa4;
-	struct sockaddr_in6 tmp;
+	struct addrinfo *res, *ai;
+	int err;
 
-	out->sin6_family = AF_INET6;
-	if (inet_pton(AF_INET6, str, &(out->sin6_addr)) == 1) {
+	res = net_client_lookup(str, NULL, AF_UNSPEC, SOCK_STREAM);
+	if (!res) {
+		warnx("Failed to resolve %s", str);
+		return -1;
+	}
+
+	memset(out, 0, sizeof(*out));
+	err = -1;
+	for (ai = res; ai; ai = ai->ai_next) {
+		if (ai->ai_family == AF_INET6)
+			memcpy(out, ai->ai_addr, sizeof(*out));
+		else if (ai->ai_family == AF_INET)
+			inet_to_inet6(ai->ai_addr, out);
+		else
+			continue;
+
 		out->sin6_family = AF_INET6;
-		return 0;
+		out->sin6_port = 0;
+		err = 0;
+		break;
 	}
+	if (err)
+		warnx("No IP address found for %s", str);
 
-	sa4 = (struct sockaddr_in *)&tmp;
-	if (inet_pton(AF_INET, str, &(sa4->sin_addr)) == 1) {
-		sa4->sin_family = AF_INET;
-		inet_to_inet6((void *)sa4, out);
-		return 0;
-	}
-
-	return -1;
+	freeaddrinfo(res);
+	return err;
 }
 
 int main(int argc, char *argv[])